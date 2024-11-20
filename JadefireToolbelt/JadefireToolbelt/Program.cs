57b492e199041dc7264ab6b986fdc23fe48ddb47
kyle-loyd <kyloyd704@gmail.com>
2024-11-20 15:21:06 -0600

    new app config, new MindfulBell, changes to Program to accomodate both

--- JadefireToolbelt/JadefireToolbelt/Program.cs
+++ JadefireToolbelt/JadefireToolbelt/Program.cs
@@ -1,23 +1,53 @@
+using JadefireToolbelt;
+using System.Configuration;
+
 class Program
 {
     [STAThread]
     static void Main()
     {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-
-        NotifyIcon trayIcon = new()
+        try
         {
-            Icon = new Icon("./jadefire.ico"),
-            Visible = true,
-            Text = "Jadefire"
-        };
+            var bellIntervalSetting = ConfigurationManager.AppSettings["MindfulBell_Interval"];
+            var bellInterval = double.TryParse(bellIntervalSetting, out var result) ? result : 0;
+            var bell = new MindfulBell(bellInterval);
 
-        var contextMenu = new ContextMenuStrip();
-        contextMenu.Items.Add("GUID to Clipboard", null, (sender, args) => Clipboard.SetText(Guid.NewGuid().ToString()));
-        contextMenu.Items.Add("Exit", null, (sender, args) => Application.Exit());
-        trayIcon.ContextMenuStrip = contextMenu;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-        Application.Run();
+            var iconTextSetting = ConfigurationManager.AppSettings["IconText"];
+            NotifyIcon trayIcon = new()
+            {
+                Icon = new Icon("./jadefire.ico"),
+                Visible = true,
+                Text = !string.IsNullOrWhiteSpace(iconTextSetting) ? iconTextSetting : "Jadefire"
+            };
+
+            var contextMenu = new ContextMenuStrip();
+            
+            contextMenu.Items.Add("GUID to Clipboard", null, (sender, args) => Clipboard.SetText(Guid.NewGuid().ToString()));
+            
+            var mindfulBellMenu = new ToolStripMenuItem("Mindful Bell");
+            mindfulBellMenu.DropDownItems.Add("Start", null, (sender, args) => bell.Start());
+            mindfulBellMenu.DropDownItems.Add("Stop", null, (sender, args) => bell.Stop());
+            
+            contextMenu.Items.Add(mindfulBellMenu);
+            
+            contextMenu.Items.Add("Exit", null, (sender, args) => Application.Exit());
+            
+            trayIcon.ContextMenuStrip = contextMenu;
+
+            Application.Run();
+        }
+
+        catch (SoundPlayFailureException spfe)
+        {
+            MessageBox.Show(spfe.Message);
+        }
+
+        catch (Exception e) 
+        {
+            MessageBox.Show(e.Message);
+        }
     }
 }
